2d58e8dd5f61ac5242ff5eb9681fcf7dd996c95b
hisanhunais <hisan.live@gmail.com>
2018-06-02 00:39:58 +0530

    Getting book data from database works

---
+++ src/app/book.ts
@@ -0,0 +1,10 @@
+export class Book {
+    _id: string;
+    isbn: Number;
+    name: String;
+    category: String;
+    author: String;
+    publisher: String;
+    edition: Number;
+    noOfCopies: Number;
+}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
+  providers: [BookService]
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+ 
 
-  ngOnInit() {
-  }
+  books: Array<Book>;
+  
+    //selectedVideo: Video;
+    //private hidenewVideo: boolean = true;
+  
+    constructor(private _bookService: BookService) { }
+  
+    ngOnInit() {
+      this._bookService.getBooks()
+        .subscribe(resBookData => this.books = resBookData);
+    }
+  
+    /*onSelectVideo(video:any){
+      this.selectedVideo = video;
+      this.hidenewVideo = true;
+      console.log(this.selectedVideo);
+    }
+  
+    onSubmitAddVideo(video: Video){
+      this._videoService.addVideo(video)
+        .subscribe(resNewVideo => {
+          this.videos.push(resNewVideo);
+          this.hidenewVideo = true;
+          this.selectedVideo = resNewVideo;
+        });
+    }
+  
+    onUpdateVideoEvent(video: any){
+      this._videoService.updateVideo(video)
+        .subscribe(resUpdatedVideo => video = resUpdatedVideo);
+      this.selectedVideo = null;
+    }
+  
+    onDeleteVideoEvent(video: any){
+      let videoArray = this.videos;
+      this._videoService.deleteVideo(video)
+        .subscribe(resDeletedVideo => {
+          for(let i=0; i < videoArray.length; i++)
+          {
+            if(videoArray[i]._id === video._id)
+            {
+              videoArray.splice(i,1);
+            }
+          }
+        });
+      this.selectedVideo = null;
+    }
+  
+    newVideo(){
+      this.hidenewVideo = false;
+    }*/
 
 }
